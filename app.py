7898f166466aea116ce1e3348a2fc573f10340b8
Heavrnl <tisandest@gmail.com>
2024-08-19 16:50:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,14 +10,13 @@ model_path = "./qwen2-0_5b-instruct-q4_k_m.gguf"
 model = Llama(model_path=model_path, n_ctx=2048, n_threads=4)
 
 
-def get_response(input_text, prompt_template):
+def get_response(input_text, prompt_template, max_tokens, temperature):
     prompt = prompt_template.format(input_text=input_text)
     print("prompt:" + prompt)
-    output = model(prompt, max_tokens=20, echo=False, temperature=0.05)
+    output = model(prompt, max_tokens=max_tokens, echo=False, temperature=temperature)
     print(f"Raw output: {output}")
     response = output['choices'][0]['text'].strip()
     print("response:" + response)
-    # 后处理逻辑
 
     # 清理内存
     gc.collect()
@@ -28,16 +27,20 @@ def get_response(input_text, prompt_template):
 @app.route('/process', methods=['POST'])
 def process_text():
     input_data = request.json.get('text')
-    prompt_template = request.json.get('prompt_template')
-    if not  prompt_template:
+    prompt_template = request.json.get('prompt_template', 'You are a helpful assistant.{input_text}')
+    max_tokens = request.json.get('max_tokens', 20)
+    temperature = request.json.get('temperature', 0.05)
+
+    if not prompt_template:
         prompt_template = "You are a helpful assistant.{input_text}"
+
     print("prompt_template:" + prompt_template)
     print("input_data:" + input_data)
 
-    if not input_data or not prompt_template:
-        return jsonify({'error': 'No input text or prompt template provided'}), 400
+    if not input_data:
+        return jsonify({'error': 'No input text provided'}), 400
 
-    response = get_response(input_data, prompt_template)
+    response = get_response(input_data, prompt_template, max_tokens, temperature)
     print("Response: " + response, flush=True)
 
     return jsonify({'response': response}), 200
